0c78d8218c4afb9c3d22c87f9c6826a7fbb12433
Anshi LeFlay <necrofallen@gmail.com>
2023-05-04 18:40:26 +0400

    email check (university list and ac&edu) -untested

--- backend/src/utils/functions.ts
+++ backend/src/utils/functions.ts
@@ -12,10 +12,48 @@ export const generateRandomPassword = (length: number) => {
     return password;
 };
 
-export const domainCheck = (email: string) => {
-    const prisma = new PrismaClient();
-    //prisma.universities
+export const domainCheck = async (email: string) => {
+    try {
+        const [, domain] = email.split("@");
+        const emailDomain = domain.split(".");
 
-    const [, domain] = email.split("@");
-    const [, tld] = domain.split(".");
+        const prisma = new PrismaClient();
+
+        const univerDB = await prisma.universities.findMany();
+
+        if (domain === undefined) return;
+
+        for (let i = 0; i < univerDB.length; i++) {
+            if (univerDB[i].Domains !== null) {
+                let needleDomain = univerDB[i].Domains!.split(".");
+
+                let findCount = 0;
+                let index = emailDomain.length - 1;
+
+                for (let i = needleDomain.length - 1; i > -1; i--) {
+                    if (needleDomain[i] === emailDomain[index]) findCount++;
+                    else break;
+                    index--;
+                }
+
+                if (findCount === needleDomain.length)
+                    return {
+                        id: univerDB[i].id,
+                        name: univerDB[i].Name,
+                        type: "university_domain",
+                    };
+            }
+        }
+
+        //проверяем .ac и .edu
+        if (emailDomain[emailDomain.length - 1] === "ac") return { type: "ac" };
+        if (emailDomain[emailDomain.length - 1] === "edu")
+            return { type: "edu" };
+
+        //проверяем whitelist
+
+        return null;
+    } catch (err: any) {
+        return { error: err.message, type: "error" };
+    }
 };
