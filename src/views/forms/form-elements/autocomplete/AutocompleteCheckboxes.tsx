1b11c2deb253603a50aed4317496a9223bb475c3
Anshi LeFlay <necrofallen@gmail.com>
2023-05-10 20:19:32 +0400

    fake-db fixes

--- src/views/forms/form-elements/autocomplete/AutocompleteCheckboxes.tsx
+++ src/views/forms/form-elements/autocomplete/AutocompleteCheckboxes.tsx
@@ -1,28 +1,35 @@
 // ** MUI Imports
-import Checkbox from '@mui/material/Checkbox'
-import TextField from '@mui/material/TextField'
-import Autocomplete from '@mui/material/Autocomplete'
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
 // ** Data
-import { top100Films } from 'src/@fake-db/autocomplete'
+//import { top100Films } from 'src/@fake-db/autocomplete'
+const top100Films: any = {};
 
 const AutocompleteCheckboxes = () => {
-  return (
-    <Autocomplete
-      multiple
-      disableCloseOnSelect
-      options={top100Films}
-      id='autocomplete-checkboxes'
-      getOptionLabel={option => option.title || ''}
-      renderInput={params => <TextField {...params} label='Checkboxes' placeholder='Favorites' />}
-      renderOption={(props, option, { selected }) => (
-        <li {...props}>
-          <Checkbox checked={selected} sx={{ mr: 2 }} />
-          {option.title}
-        </li>
-      )}
-    />
-  )
-}
+    return (
+        <Autocomplete
+            multiple
+            disableCloseOnSelect
+            options={top100Films}
+            id="autocomplete-checkboxes"
+            getOptionLabel={(option: any) => option.title || ""}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Checkboxes"
+                    placeholder="Favorites"
+                />
+            )}
+            renderOption={(props, option, { selected }) => (
+                <li {...props}>
+                    <Checkbox checked={selected} sx={{ mr: 2 }} />
+                    {option.title}
+                </li>
+            )}
+        />
+    );
+};
 
-export default AutocompleteCheckboxes
+export default AutocompleteCheckboxes;
--- src/views/forms/form-elements/autocomplete/AutocompleteCustomFilter.tsx
+++ src/views/forms/form-elements/autocomplete/AutocompleteCustomFilter.tsx
@@ -1,30 +1,34 @@
 // ** MUI Imports
-import TextField from '@mui/material/TextField'
-import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete'
+import TextField from "@mui/material/TextField";
+import Autocomplete, { createFilterOptions } from "@mui/material/Autocomplete";
 
 // ** Data
-import { top100Films } from 'src/@fake-db/autocomplete'
+//import { top100Films } from 'src/@fake-db/autocomplete'
+
+const top100Films: any = {};
 
 interface FilmOptionType {
-  year: number
-  title: string
+    year: number;
+    title: string;
 }
 
 const filterOptions = createFilterOptions({
-  matchFrom: 'start',
-  stringify: (option: FilmOptionType) => option.title
-})
+    matchFrom: "start",
+    stringify: (option: FilmOptionType) => option.title,
+});
 
 const AutocompleteCustomFilter = () => {
-  return (
-    <Autocomplete
-      options={top100Films}
-      filterOptions={filterOptions}
-      id='autocomplete-custom-filter'
-      getOptionLabel={option => option.title || ''}
-      renderInput={params => <TextField {...params} label='Custom filter' />}
-    />
-  )
-}
+    return (
+        <Autocomplete
+            options={top100Films}
+            filterOptions={filterOptions}
+            id="autocomplete-custom-filter"
+            getOptionLabel={(option) => option.title || ""}
+            renderInput={(params) => (
+                <TextField {...params} label="Custom filter" />
+            )}
+        />
+    );
+};
 
-export default AutocompleteCustomFilter
+export default AutocompleteCustomFilter;
